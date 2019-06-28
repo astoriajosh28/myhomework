d4c00cc977cb6901c88d23122bae0ecdffbf5d57
astoriajosh28 <49353983+astoriajosh28@users.noreply.github.com>
2019-06-28 15:49:12 -0400

    Add files via upload

---
+++ HW#9/SQL Queries.sql
@@ -0,0 +1,5 @@
+--1. List the following details of each employee: employee number, last name, first name, gender, and salary.
+
+SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
+FROM employees, salaries
+WHERE salaries.emp_no = employees.emp_no;
