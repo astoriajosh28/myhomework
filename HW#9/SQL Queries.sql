b49675bfc22449e0d98296fcf5c716ae6b9d1200
astoriajosh28 <49353983+astoriajosh28@users.noreply.github.com>
2019-06-28 16:18:14 -0400

    Add files via upload

--- HW#9/SQL Queries.sql
+++ HW#9/SQL Queries.sql
@@ -3,3 +3,9 @@
 SELECT employees.emp_no, employees.last_name, employees.first_name, employees.gender, salaries.salary
 FROM employees, salaries
 WHERE salaries.emp_no = employees.emp_no;
+
+-- 2. List employees who were hired in 1986.
+
+SELECT employees.last_name, employees.first_name, employees.hire_date
+FROM employees
+WHERE hire_date between '1985-12-31' and '1987-01-01';
