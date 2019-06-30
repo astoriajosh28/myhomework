1699b60b5f58b3a33aeb0f225317f7794331f869
astoriajosh28 <49353983+astoriajosh28@users.noreply.github.com>
2019-06-30 19:18:36 -0400

    Add files via upload

--- HW#9/SQL Queries.sql
+++ HW#9/SQL Queries.sql
@@ -40,3 +40,20 @@ Where first_name = 'Hercules' AND last_name Like 'B%'
 SELECT employees.emp_no, departments.dept_name, employees.last_name, employees.first_name
 FROM departments, employees
 Where dept_name = 'Sales'
+
+-- 7. List all employees in the Sales and Development departments,
+-- including their employee number, last name, first name, and department name.
+
+SELECT dept_emp.emp_no, employees.last_name, employees.first_name, departments.dept_name
+FROM dept_emp
+LEFT JOIN employees ON dept_emp.emp_no = employees.emp_no
+LEFT JOIN departments ON dept_emp.dept_no = departments.dept_no
+WHERE dept_name = 'Sales' OR dept_name = 'Development';
+
+-- 8. In descending order, list the frequency count of
+-- employee last names, i.e., how many employees share each last name.
+
+SELECT last_name, COUNT(*)
+FROM employees
+GROUP BY last_name
+ORDER BY count(*) DESC;
